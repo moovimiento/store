783cfdd5e82001b8b7735b160d5713e5dad1fa0d
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-06-26 03:53:20 -0300

    33

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -6,14 +6,14 @@ export const tiers: IPricing[] = [
     price: 3300,
     features: ["Pasas de uva", "Almendras", "Nueces", "Peras deshidratadas"],
     whatsappLink:
-      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+la+C%C3%A1psula+de+Frutos+Secos+de+%243000+%F0%9F%A5%9C%EF%B8%8F%E2%9A%A1%EF%B8%8F%F0%9F%8C%B1&type=phone_number&app_absent=0",
+      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+la+C%C3%A1psula+de+Frutos+Secos+de+%243300+%F0%9F%A5%9C%EF%B8%8F%E2%9A%A1%EF%B8%8F%F0%9F%8C%B1&type=phone_number&app_absent=0",
   },
   {
     name: "5 Cápsulas = 750g",
-    price: 12500,
+    price: 13300,
     features: ["Pasas de uva", "Almendras", "Nueces", "Peras deshidratadas"],
     whatsappLink:
-      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+la+promo+de+5+C%C3%A1psulas+de+Frutos+Secos+por+%2412500+%F0%9F%A5%9C%EF%B8%8F%E2%9A%A1%EF%B8%8F%F0%9F%8C%B1&type=phone_number&app_absent=0",
+      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+la+promo+de+5+C%C3%A1psulas+de+Frutos+Secos+por+%2413300+%F0%9F%A5%9C%EF%B8%8F%E2%9A%A1%EF%B8%8F%F0%9F%8C%B1&type=phone_number&app_absent=0",
   },
   {
     name: "Promo de +15 Cápsulas",

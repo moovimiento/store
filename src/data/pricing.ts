639337718a82e850002a48ddcce85b1c8bef9eeb
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-06-26 01:30:57 -0300

    Improvement

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -2,21 +2,21 @@ import { IPricing } from "@/types";
 
 export const tiers: IPricing[] = [
   {
-    name: "Cápsula de 150g",
+    name: "1 Cápsula = 150g",
     price: 3000,
     features: ["Pasas de uva", "Almendras", "Nueces", "Peras deshidratadas"],
     whatsappLink:
-      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+una+C%C3%A1psula+de+150g+por+%242500+%F0%9F%A5%9C%EF%B8%8F%E2%9A%A1%EF%B8%8F%F0%9F%8C%B1&type=phone_number&app_absent=0",
+      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+la+C%C3%A1psula+de+Frutos+Secos+de+%243000+%F0%9F%A5%9C%EF%B8%8F%E2%9A%A1%EF%B8%8F%F0%9F%8C%B1&type=phone_number&app_absent=0",
   },
   {
-    name: "Pack de 5 Cápsulas (750g)",
+    name: "5 Cápsulas = 750g",
     price: 12500,
     features: ["Pasas de uva", "Almendras", "Nueces", "Peras deshidratadas"],
     whatsappLink:
-      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+el+Pack+de+5+C%C3%A1psulas+por+%2410000+%F0%9F%92%BC%F0%9F%94%A5%F0%9F%A5%AD&type=phone_number&app_absent=0",
+      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Quiero+la+promo+de+5+C%C3%A1psulas+de+Frutos+Secos+por+%2412500+%F0%9F%A5%9C%EF%B8%8F%E2%9A%A1%EF%B8%8F%F0%9F%8C%B1&type=phone_number&app_absent=0",
   },
   {
-    name: "Pack de +15 Cápsulas",
+    name: "Promo de +15 Cápsulas",
     price: "Personalizado",
     features: [
       "Pasas de uva",
@@ -26,6 +26,6 @@ export const tiers: IPricing[] = [
       "+ Frutos secos a elección",
     ],
     whatsappLink:
-      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Estoy+interesado%2Fa+en+el+Pack+de+%2B15+C%C3%A1psulas+y+quisiera+consultar+el+precio+%F0%9F%93%A6%EF%B8%8F%E2%9C%A8%F0%9F%8D%87&type=phone_number&app_absent=0",
+      "https://api.whatsapp.com/send/?phone=543513239624&text=Hola%21+Estoy+interesado%2Fa+en+la+promo+de+%2B15+C%C3%A1psulas+%F0%9F%93%A6%EF%B8%8F%E2%9C%A8%F0%9F%8D%87+Quisiera+consultar+el+precio&type=phone_number&app_absent=0",
   },
 ];
